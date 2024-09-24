2f4f27e67f042baba435fec076cf840f0f93784b
callivan <ivam-my-job@mail.ru>
2024-09-24 20:06:29 +0700

    [feat] Добавляет основную страницу и страницу настроек

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,36 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnSearch = findViewById<Button>(R.id.search)
+        val btnMedia = findViewById<Button>(R.id.media)
+        val btnSettings = findViewById<Button>(R.id.settings)
+
+        val btnSearchClickListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val intent = Intent(v?.context, SearchActivity::class.java)
+
+                startActivity(intent)
+            }
+        }
+
+        btnSearch.setOnClickListener(btnSearchClickListener)
+
+        btnMedia.setOnClickListener {
+            val intent = Intent(
+                this, MediaActivity::class.java
+            )
+
+            startActivity(intent)
+        }
+
+        btnSettings.setOnClickListener {
+            val intent = Intent(
+                this, SettingsActivity::class.java
+            )
+
+            startActivity(intent)
         }
     }
 }

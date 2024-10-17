8c2c3c347a6ed057deb10acba2f7f83178221da6
callivan <ivam-my-job@mail.ru>
2024-10-17 23:46:54 +0700

    [feat] Добавляет список песен (сбросил кэш)

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -4,18 +4,99 @@ import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.google.android.material.appbar.MaterialToolbar
+
+data class Track(
+    val trackName: String,
+    val trackTime: String,
+    val artistName: String,
+    val artWorkUrl100: String
+)
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val trackName: TextView = itemView.findViewById(R.id.trackName)
+    private val trackArtist: TextView = itemView.findViewById(R.id.trackArtist)
+    private val trackTime: TextView = itemView.findViewById(R.id.trackTime)
+    private val trackPreview: ImageView = itemView.findViewById(R.id.trackPreview)
+
+    private val glide = Glide.with(itemView)
+
+    fun bind(model: Track) {
+        trackName.text = model.trackName
+        trackArtist.text = model.artistName
+        trackTime.text = model.trackTime
+        glide.load(model.artWorkUrl100).into(trackPreview)
+    }
+
+}
+
+class TrackAdapter(
+    private val news: List<Track>
+) : RecyclerView.Adapter<TrackViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
+        holder.bind(news[position])
+    }
+
+    override fun getItemCount(): Int {
+        return news.size
+    }
+}
 
 class SearchActivity : AppCompatActivity() {
     private var searchText = ""
+    private val tracks: List<Track> = listOf(
+        Track(
+            "Smells Like Teen Spirit",
+            "5:01",
+            "Nirvana",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Billie Jean",
+            "4:35",
+            "Michael Jackson",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Stayin' Alive",
+            "4:10",
+            "Bee Gees",
+            "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Whole Lotta Love",
+            "5:33",
+            "Led Zeppelin",
+            "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"
+        ),
+        Track(
+            "Sweet Child O'Mine",
+            "5:03",
+            "Guns N' Roses",
+            "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"
+        )
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +111,9 @@ class SearchActivity : AppCompatActivity() {
         val btnBack = findViewById<MaterialToolbar>(R.id.back)
         val inputSearch = findViewById<EditText>(R.id.inputSearch)
         val btnClean = findViewById<ImageButton>(R.id.btnClean)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+
+        recyclerView.adapter = TrackAdapter(tracks)
 
         btnBack.setNavigationOnClickListener {
             finish()
